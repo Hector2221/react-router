4f05f544295e0cae437ee0453f87a70d036af622
Hector Alejandro <hector.alejandro2221@gmail.com>
2023-05-21 18:34:20 -0600

    proyecto terminado

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
+        <Route path="/posts/:id" element={<Post />} />
+        <Route path="/categoria/:id/*" element={<Categoria />} />
         <Route path="/*" element={<Page404 />} />
       </Routes>
     </Router>
